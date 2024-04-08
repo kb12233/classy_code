7e8fede383778481501e09e1691303a2188b5f1b
AshleyMoriahLazaraga <ama.lazaraga@gmail.com>
2024-04-08 20:17:57 +0800

    added logo in registration page

--- lib/views/registration.dart
+++ lib/views/registration.dart
@@ -20,34 +20,59 @@ class _RegistrationPageState extends State<RegistrationPage> {
         child: Row(
           children: [
             Expanded(
-              flex: 1,
+              flex: 2,
               child: Container(
                 color: Color(0xFFB8DBD9), // Left color
                 alignment: Alignment.centerLeft,
-                child: SingleChildScrollView(
-                  padding: const EdgeInsets.all(16.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        'Welcome Back!',
-                        style: TextStyle(
-                          color: Color(0xFF31363F),
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: GoogleFonts.jetBrainsMono().fontFamily,
-                        ), // Set font to JetBrains Mono
+                      Row(
+                        children: [
+                          Image.asset(
+                            'lib/images/logo_light.png',
+                            width: 50,
+                            height: 50,
+                          ),
+                          SizedBox(width: 20), // Adjust spacing
+                          // App name
+                          Text(
+                            "ClassyCode",
+                            style: GoogleFonts.jetBrainsMono(
+                              color: Color(0xFF202124),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 25.0),
-                      Text(
-                        'Stay in touch! Sign in with your info.',
-                        style: TextStyle(
-                          color: Color(0xFF31363F),
-                          fontSize: 22,
-                          fontFamily: GoogleFonts.jetBrainsMono().fontFamily,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 150.0),
+                        child: Text(
+                          'Welcome Back!',
+                          style: TextStyle(
+                            color: Color(0xFF31363F),
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.jetBrainsMono().fontFamily,
+                          ), // Set font to JetBrains Mono
+                        ),
+                      ),
+                      SizedBox(height: 50.0),
+                      Padding(
+                        padding: EdgeInsets.only(left: 90, right: 50),
+                        child: Text(
+                          'Stay in touch! Sign in with your info.',
+                          style: TextStyle(
+                            color: Color(0xFF31363F),
+                            fontSize: 22,
+                            fontFamily: GoogleFonts.jetBrainsMono().fontFamily,
+                          ),
                         ),
                       ), // New text added
-                      const SizedBox(height: 25.0), // Adjust spacing as needed
+                      const SizedBox(height: 30.0), // Adjust spacing as needed
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
@@ -80,7 +105,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
               ),
             ),
             Expanded(
-              flex: 1,
+              flex: 3,
               child: Container(
                 color: Color(0xFF202124), // Right color
                 alignment: Alignment.centerLeft,
@@ -230,7 +255,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
                                     0xFFB8DBD9), // Set text color to #B8DBD9
                                 fontFamily:
                                     GoogleFonts.jetBrainsMono().fontFamily,
-                              ), // Set font to JetBrains Mono
+                              ),
                             ),
                             style: ElevatedButton.styleFrom(
                               minimumSize: const Size(150, 50),
